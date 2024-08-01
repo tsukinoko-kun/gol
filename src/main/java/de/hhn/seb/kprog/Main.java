967e595aab8926dfa0ca212f3e0b9a1bd550c4ba
Frank Mayer <gh@frankmayer.dev>
2024-08-01 22:10:58 +0200

    use canvas

--- src/main/java/de/hhn/seb/kprog/Main.java
+++ src/main/java/de/hhn/seb/kprog/Main.java
@@ -4,24 +4,34 @@ package de.hhn.seb.kprog;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     // static values
     public final static int WORLD_SIZE = 512;
     private final static int RECTANGLE_SIZE = 1;
+    private final static int DISPLAY_SIZE = WORLD_SIZE * RECTANGLE_SIZE;
 
     // components
     private final World world = new World(Main.WORLD_SIZE);
     private final Rules rules = new Rules(this.world);
-    private final Rectangle[] rectangles = new Rectangle[Main.WORLD_SIZE * Main.WORLD_SIZE];
+    private final Canvas canvas = new Canvas(Main.WORLD_SIZE*Main.RECTANGLE_SIZE, Main.WORLD_SIZE*Main.RECTANGLE_SIZE);
+    private final GraphicsContext gc = canvas.getGraphicsContext2D();
+    private final WritableImage buffer = new WritableImage(Main.WORLD_SIZE, Main.WORLD_SIZE);
+    private final PixelWriter pixelWriter = buffer.getPixelWriter();
     private final Benchmark benchmark = Benchmark.getInstance();
     private final Label benchmarkLabel = new Label();
+
+    public Main() {
+        this.gc.setImageSmoothing(false);
+    }
 
     public static void main(String[] args) {
         Application.launch(args);
@@ -32,21 +42,10 @@ public class Main extends Application {
         primaryStage.setTitle("Game of Life");
         VBox root = new VBox();
         root.getChildren().add(benchmarkLabel);
+        root.getChildren().add(this.canvas);
         int sceneSize = Main.WORLD_SIZE * Main.RECTANGLE_SIZE;
         Scene scene = new Scene(root, sceneSize, sceneSize + 50);
 
-        GridPane grid = new GridPane();
-        root.getChildren().add(grid);
-
-        for (int y = 0; y < Main.WORLD_SIZE; y++) {
-            for (int x = 0; x < Main.WORLD_SIZE; x++) {
-                int i = y * Main.WORLD_SIZE + x;
-                Rectangle rectangle = new Rectangle(Main.RECTANGLE_SIZE, Main.RECTANGLE_SIZE);
-                rectangle.setFill(Color.BLACK);
-                grid.add(rectangle, x, y);
-                this.rectangles[i] = rectangle;
-            }
-        }
 
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
@@ -68,10 +67,10 @@ public class Main extends Application {
         this.benchmarkLabel.setText(this.benchmark.getText());
         for (int y = 0; y < Main.WORLD_SIZE; y++) {
             for (int x = 0; x < Main.WORLD_SIZE; x++) {
-                int i = y * Main.WORLD_SIZE + x;
-                this.rectangles[i].setFill(this.world.isAlive(x, y) ? Color.DARKGREEN : Color.BLACK);
+                this.pixelWriter.setColor(x, y, this.world.isAlive(x, y) ? Color.DARKGREEN : Color.BLACK);
             }
         }
+        this.gc.drawImage(this.buffer, 0, 0, Main.DISPLAY_SIZE, Main.DISPLAY_SIZE);
         this.benchmark.stop();
     }
 }
